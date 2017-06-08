7ee5b7ad39f9701c7fc5a105e8a46f7f490dac52
shehryar <shehryar@Shehryar-MacBook-Pro.local>
2017-06-09 01:58:30 +0200

    PaymentEmployerActivity
    .java titles and buttons

--- app/src/main/java/com/androidgroup/godelivery/PaymentEmployerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PaymentEmployerActivity.java
@@ -44,6 +44,41 @@ public class PaymentEmployerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_job_employer);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "FancyFont_1.ttf");
+
+        paymentDescription = (TextView) findViewById(R.id.PaymentJobEmployerDescriptionID);
+
+        paymentTitle = (TextView) findViewById(R.id.PaymentJobEmployerTitleID);
+
+        payButton = (Button) findViewById(R.id.PaymentJobEmployerButtonID);
+
+        LogOutButton = (Button) findViewById(R.id.LogoutButtonID);
+
+        LogOutButton.setTypeface(font);
+        LogOutButton.setTextColor(Color.WHITE);
+
+        RefreshButton = (Button) findViewById(R.id.RefreshButtonID);
+
+        RefreshButton.setTypeface(font);
+        RefreshButton.setTextColor(Color.WHITE);
+
+
+
+        paymentTitle.setTypeface(font);
+        paymentTitle.setTextColor(Color.WHITE);
+
+        paymentDescription.setTypeface(font);
+        paymentDescription.setTextColor(Color.WHITE);
+
+        payButton.setTypeface(font);
+        payButton.setTextColor(Color.WHITE);
+
+
+
+
+
+
     }
 
 
