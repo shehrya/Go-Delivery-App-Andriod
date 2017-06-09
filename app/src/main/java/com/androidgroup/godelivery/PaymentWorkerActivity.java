c603371c266228589a99b8bf010c10d7d3b30673
shehryar <shehryar@Shehryar-MacBook-Pro.local>
2017-06-10 01:02:44 +0200

    Payment Worker Activity
    .java file ,libraries imported

--- app/src/main/java/com/androidgroup/godelivery/PaymentWorkerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PaymentWorkerActivity.java
@@ -3,6 +3,22 @@ package com.androidgroup.godelivery;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class PaymentWorkerActivity extends Activity {
 
     @Override
