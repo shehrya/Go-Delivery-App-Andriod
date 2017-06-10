2247be8e508393ad621bc9b9c7653fe94e374437
muhammadumairgul <muhammadumairgul@yahoo.com>
2017-06-10 14:31:01 +0200

    Already Ready Login
    .java import libraries

--- app/src/main/java/com/androidgroup/godelivery/AlreadyLoggedInActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/AlreadyLoggedInActivity.java
@@ -3,6 +3,14 @@ package com.androidgroup.godelivery;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 public class AlreadyLoggedInActivity extends Activity {
 
     @Override
