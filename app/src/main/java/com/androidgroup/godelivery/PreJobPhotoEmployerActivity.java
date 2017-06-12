87fcdd633a1c9b42f8f87d7ff29c3639b61853f6
shehryar <shehryar@Shehryar-MacBook-Pro.local>
2017-06-12 15:54:47 +0200

    Pre Job Photo Employer Activity
    .java file librarie import

--- app/src/main/java/com/androidgroup/godelivery/PreJobPhotoEmployerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PreJobPhotoEmployerActivity.java
@@ -3,6 +3,35 @@ package com.androidgroup.godelivery;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.util.ByteArrayBuffer;
+
+
 public class PreJobPhotoEmployerActivity extends Activity {
 
 
