7695ceaecd9c2338473a38145162db2f3d56a04f
shehryar <shehryar@Shehryar-MacBook-Pro.local>
2017-06-12 16:20:20 +0200

    Pre Job Photo Worker Activity
    .Java class libraries import

--- app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
@@ -3,6 +3,18 @@ package com.androidgroup.godelivery;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.AsyncTask;
 public class PreJobPhotoWorkerActivity extends Activity {
 
     @Override
