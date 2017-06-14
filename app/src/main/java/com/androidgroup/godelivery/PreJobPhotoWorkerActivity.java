624ce808be216f92659e702c4fd41d3e2e2c0464
shehryar <shehryar@Shehryar-MacBook-Pro.local>
2017-06-15 00:20:57 +0200

    Pre Job Photo Worker Activity
    .Java library import

--- app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
@@ -14,7 +14,34 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
-import android.os.AsyncTask;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.util.ByteArrayBuffer;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class PreJobPhotoWorkerActivity extends Activity {
 
     @Override
