ef4773d0df4bba0650490fc9992c90d8134c1e49
shehryar <shehryar@Shehryar-MacBook-Pro.local>
2017-06-15 00:38:39 +0200

    Pre Job Photo Worker Activity
    .Java pre job photo worker activity finished

--- app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
@@ -629,10 +629,137 @@ public class PreJobPhotoWorkerActivity extends Activity {
     }
 
 
+    private String FetchJobDetails(String myurl) throws IOException, UnsupportedEncodingException {
+        InputStream is = null;
+
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setUseCaches(false);
+            conn.setDefaultUseCaches(false);
+            conn.addRequestProperty("Cache-Control", "no-cache");
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
 
 
 
-}
+            is = conn.getInputStream();
+
+            BufferedReader textReader = new BufferedReader(new InputStreamReader(is));
+
+
+            String readlineTextListing;
+            String complexJobListingString= null;
+
+
+
+
+            while ((readlineTextListing = textReader.readLine()) != null) {
+
+                complexJobListingString = readlineTextListing;
+
+
+                if(complexJobListingString.length() > 25) {
+
+
+
+                    int counter = 0;
+
+
+                    for (int i = 0; i < complexJobListingString.length(); ++i) {
+
+                        if (complexJobListingString.charAt(i) == '|') {
+                            ++counter;
+                            continue;
+                        }
+
+
+                        jobDetails[counter] = jobDetails[counter] + complexJobListingString.charAt(i);
+
+
+                    }
+
+
+                    break;
+
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+            {
+                return "OK";
+            }
+            else
+            {
+                return "NetworkError";
+            }
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+
+
+            if (is != null)
+            {
+                is.close();
+
+
+            }
+
+        }
+    }
+
+
+    public void LogOutClicked(View v)
+    {
+        LogoutUser();
+
+        Intent intent = new Intent(PreJobPhotoWorkerActivity.this, AlreadyLoggedInActivity.class);
+
+        startActivity(intent);
+
+        finish();
+
+    }
+
+    public void LogoutUser()
+    {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("GoDeliveryLoginEmail", null);
+        editor.apply();
+    }
+
+    public void RefreshClicked(View v)
+    {
+        Intent intent = new Intent(PreJobPhotoWorkerActivity.this, AlreadyLoggedInActivity.class);
+
+        startActivity(intent);
+
+        finish();
+
+
+
+
+    }
 
 
 }
